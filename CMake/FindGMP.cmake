360820dd981fa72117f255ddaa68367419f7526c
Qingnan Zhou <qzhou@adobe.com>
2018-09-11 08:47:59 -0700

    Update cmake setup.

--- CMake/FindGMP.cmake
+++ CMake/FindGMP.cmake
@@ -1,22 +1,19 @@
-# Try to find the GNU Multiple Precision Arithmetic Library (GMP)
-# See http://gmplib.org/
+# Try to find the GMP librairies
+# GMP_FOUND - system has GMP lib
+# GMP_INCLUDE_DIRS - the GMP include directory
+# GMP_LIBRARIES - Libraries needed to use GMP
 
-if (GMP_INCLUDES AND GMP_LIBRARIES)
-  set(GMP_FIND_QUIETLY TRUE)
-endif (GMP_INCLUDES AND GMP_LIBRARIES)
+if (GMP_INCLUDE_DIRS AND GMP_LIBRARIES)
+        # Already in cache, be silent
+        set(GMP_FIND_QUIETLY TRUE)
+endif (GMP_INCLUDE_DIRS AND GMP_LIBRARIES)
 
-find_path(GMP_INCLUDES
-  NAMES
-  gmp.h
-  PATHS
-  $ENV{GMPDIR}
-  $ENV{GMP_INC}
-  ${INCLUDE_INSTALL_DIR}
-)
-
-find_library(GMP_LIBRARIES gmp PATHS $ENV{GMPDIR} $ENV{GMP_LIB} ${LIB_INSTALL_DIR})
+find_path(GMP_INCLUDE_DIRS NAMES gmp.h PATHS $ENV{GMP_INC})
+find_library(GMP_LIBRARIES NAMES gmp libgmp PATHS $ENV{GMP_LIB})
+find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx PATHS $ENV{GMP_LIB})
+#MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG
-                                  GMP_INCLUDES GMP_LIBRARIES)
-mark_as_advanced(GMP_INCLUDES GMP_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIRS GMP_LIBRARIES)
+
+mark_as_advanced(GMP_INCLUDE_DIRS GMP_LIBRARIES)
